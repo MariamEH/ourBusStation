ee266b7328014f34161433a215f22ae61bd09e8b
SaraKhaledAtef <45892388+SaraKhaledAtef@users.noreply.github.com>
2019-04-12 03:23:54 +0200

    Algorithm of Delete is Correct all is lest is to get value from choice box

--- BusStation/src/application/ManagerAddTripController.java
+++ BusStation/src/application/ManagerAddTripController.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,8 +16,27 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ManagerAddTripController {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/ourdatabase";
+	static final String USER = "root";
+	static final String PASS = "Stay_Strong1";
+	ObservableList<String> tripBoxlist = FXCollections.observableArrayList("nonstop","one-stop","many stops");
+	ObservableList<String> ticketBoxlist = FXCollections.observableArrayList("one way","round");
+	ObservableList<String> vehicleBoxlist = FXCollections.observableArrayList("bus","train","plane","ship","car");
+	String selectedTicket;
+	String selectedTrip;
+	String selectedVehicle;
+	String id;
+	String source;
+	String destination;
+
 	@FXML
     private TextField tripIdText;
 
@@ -26,14 +47,14 @@ public class ManagerAddTripController {
     private TextField destinationText;
 
     @FXML
-    private ChoiceBox<?> ticketBox;
+    private ChoiceBox<String> ticketBox;
 
     @FXML
-    private ChoiceBox<?> tripBox;
+    private ChoiceBox<String> tripBox;
 
     @FXML
-    private ChoiceBox<?> vehicleBox;
-    
+    private ChoiceBox<String> vehicleBox;
+
     @FXML
     private Text msgText;
 
@@ -43,26 +64,50 @@ public class ManagerAddTripController {
 
     @FXML
     private Button backButton;
-    
+
+    @FXML
+    private TextField priceText;
+
     @FXML
     private RadioButton externalButton;
 
     @FXML
     private RadioButton internalButton;
-    
+    boolean isExternal = false;
+    boolean isInternal = false;
     @FXML
     void handleInternalButton(ActionEvent event) {
 
+    	isExternal=false;
+    	isInternal=true;
     }
 
     @FXML
     void handleexternalButton(ActionEvent event) {
+
+    	isExternal=true;
+    	isInternal=false;
+    }
+    @FXML
+    void initialize(){
+    	tripBox.setItems(tripBoxlist);
+    	ticketBox.setItems(ticketBoxlist);
+    	vehicleBox.setItems(vehicleBoxlist);
+
 
     }
 
 
     @FXML
     void handleAddButton(ActionEvent event) {
+    	selectedVehicle = vehicleBox.getSelectionModel().getSelectedItem();
+    	selectedTicket = ticketBox.getSelectionModel().getSelectedItem();
+    	selectedTrip = tripBox.getSelectionModel().getSelectedItem();
+    	id = tripIdText.getText();
+    	source =sourceText.getText();
+    	destination =destinationText.getText();
+    	add();
+
 
     }
 
@@ -76,5 +121,59 @@ public class ManagerAddTripController {
         appStage.setScene(home_page_scene);
         appStage.show();
     }
+    public void add() {
+    	Connection conn = null;
+    	   Statement stmt = null;
+    	   try{
+
+    	       Class.forName("com.mysql.cj.jdbc.Driver");
+
+    	      //STEP 3: Open a connection
+    	      System.out.println("Connecting to database...");
+
+    	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+    	      //STEP 4: Execute a query
+    	      System.out.println("Creating statement...");
+    	      stmt = conn.createStatement();
+           String query = "insert into externalTrips(id,source,destination,ticket,trip,vehicle,price)values(?,?,?,?,?,?,?)";
+           PreparedStatement ps = conn.prepareStatement(query);
+           ps.setString(1,tripIdText.getText());
+           ps.setString(2,sourceText.getText());
+           ps.setString(3,destinationText.getText());
+           ps.setString(4,ticketBox.getSelectionModel().getSelectedItem());
+           ps.setString(5, tripBox.getSelectionModel().getSelectedItem());
+           ps.setString(6,vehicleBox.getSelectionModel().getSelectedItem());
+           ps.setString(7,priceText.getText());
+           ps.execute();
+           stmt.executeUpdate(query);
+           System.out.println("done successfuly");
+           msgText.setText("Done adding ");
+
+    	    //STEP 6: Clean-up environment
+
+    	      conn.close();
+    	   }catch(SQLException se){
+    	      //Handle errors for JDBC
+    	      se.printStackTrace();
+    	   }catch(Exception e){
+    	      //Handle errors for Class.forName
+    	      e.printStackTrace();
+    	   }finally{
+    	      //finally block used to close resources
+    	      try{
+    	         if(stmt!=null)
+    	            stmt.close();
+    	      }catch(SQLException se2){
+    	      }// nothing we can do
+    	      try{
+    	         if(conn!=null)
+    	            conn.close();
+    	      }catch(SQLException se){
+    	         se.printStackTrace();
+    	      }//end finally try
+    	   }//end try
+    	   System.out.println("Goodbye!");
+           }//end main
+    	//end FirstExample
 
 }
--- BusStation/src/application/ManagerRemoveTripController.java
+++ BusStation/src/application/ManagerRemoveTripController.java
@@ -1,7 +1,10 @@
 package application;
 
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +17,11 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ManagerRemoveTripController {
-
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/ourdatabase?allowMultiQueries=true";
+	static final String USER = "root";
+	static final String PASS = "Stay_Strong1";
+	int i =1 ;
 	@FXML
     private Text tripsText;
 
@@ -43,7 +50,57 @@ public class ManagerRemoveTripController {
 
     @FXML
     void handleRemoveButton(ActionEvent event) {
-
+    	see();
     }
 
+    public void see() {
+    	Connection conn = null;
+    	   Statement stmt = null;
+    	   try{
+              
+    	       Class.forName("com.mysql.cj.jdbc.Driver");
+
+    	      //STEP 3: Open a connection
+    	      System.out.println("Connecting to database...");
+
+    	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+    	      //STEP 4: Execute a query
+    	      System.out.println("Creating statement...");
+    	      stmt = conn.createStatement();
+              String sql;
+              int x;
+             sql= "DELETE FROM inner_trips WHERE id= "+i;
+              x=stmt.executeUpdate(sql);
+       if(x>0)
+    	   System.out.println("Succefully deleted element of id : "+i);
+       else 
+    	   System.out.println("There is no element of id : "+i);
+       
+
+    	    //STEP 6: Clean-up environment
+    	      stmt.close();
+    	      conn.close();
+    	   }catch(SQLException se){
+    	      //Handle errors for JDBC
+    	      se.printStackTrace();
+    	   }catch(Exception e){
+    	      //Handle errors for Class.forName
+    	      e.printStackTrace();
+    	   }finally{
+    	      //finally block used to close resources
+    	      try{
+    	         if(stmt!=null)
+    	            stmt.close();
+    	      }catch(SQLException se2){
+    	      }// nothing we can do
+    	      try{
+    	         if(conn!=null)
+    	            conn.close();
+    	      }catch(SQLException se){
+    	         se.printStackTrace();
+    	      }//end finally try
+    	   }//end try
+    	   System.out.println("Goodbye!");
+           }//end main
+    	//end FirstExample
 }
